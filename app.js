d31047971401bdb7a94f6bc7f80622536d20cbab
Bradey Merrill <bradage.merrill@gmail.com>
2021-09-27 21:53:14 -0600

    working with express

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
-const http = require('http');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
 
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
+const app = express();
+
+// const mainRoutes = require('./routes/index');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(mainRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-server.listen(3000);
+// challenge express.js
+/*
+    // app.use((req, res, next) => {
+    //     console.log('first middleware');
+    //     next();
+    // });
+
+    // app.use((req, res, next) => {
+    //     console.log('second middleware');
+    //     res.send('<p>Assignment solved (almost!)</p>');
+    // });
+
+    // app.use('/users', (req, res, next) => {
+    //     console.log('/users middleware');
+    //     res.send('<p>The Middleware that handles just users</p>');
+    // });
+
+    // app.use('/', (req, res, next) => {
+    // console.log('/ middleware');
+    // res.send('<p>The Middleware that handles just /</p>');
+    // });
+*/
+
+app.listen(3000);
