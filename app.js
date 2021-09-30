c4d258293cf3780a82c019f3bff2e7051a0e5579
Bradey Merrill <bradage.merrill@gmail.com>
2021-09-29 22:47:17 -0600

    MVC complete

--- app.js
+++ app.js
@@ -2,26 +2,14 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-/*
-
-    needed only if using handle bars
-
-*/
-// const expressHbs = require('express-handlebars'); 
-// app.engine('hbs', expressHbs({
-//     layoutsDir: 'views/layouts/',
-//     defaultLayout: 'main-layout'
-// }));
-
 const app = express();
 
 app.set('view engine', 'ejs');
-// app.set('view engine', 'pug');
 app.set('views', 'views'); //where to find the templates (views)
 
-// const mainRoutes = require('./routes/index');
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const statusCodeController = require('./controllers/statusCodes');
 
 
 app.use(bodyParser.urlencoded({
@@ -29,38 +17,10 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(mainRoutes);
-app.use('/admin', adminData.routes);
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', {
-        docTitle: '404 Not Found'
-    });
-});
-
-// challenge express.js
-/*
-    // app.use((req, res, next) => {
-    //     console.log('first middleware');
-    //     next();
-    // });
-
-    // app.use((req, res, next) => {
-    //     console.log('second middleware');
-    //     res.send('<p>Assignment solved (almost!)</p>');
-    // });
-
-    // app.use('/users', (req, res, next) => {
-    //     console.log('/users middleware');
-    //     res.send('<p>The Middleware that handles just users</p>');
-    // });
-
-    // app.use('/', (req, res, next) => {
-    // console.log('/ middleware');
-    // res.send('<p>The Middleware that handles just /</p>');
-    // });
-*/
+app.use(statusCodeController.get404);
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,29 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
+router.get('/add-product', productsController.getAddProduct);
 
-router.get('/add-product', (req, res, next) => {
-    // console.log('In another middleware!');
-    //res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-    res.render('admin/add-product', {
-        docTitle: 'Add Product',
-        path: '/admin/add-product',
-        productCss: false,
-        page: 'add-product'
-    });
-});
+router.post('/add-product', productsController.postAddProduct);
 
-router.post('/add-product', (req, res, next) => {
-    products.push({
-        title: req.body.title
-    });
-    res.redirect('/');
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
