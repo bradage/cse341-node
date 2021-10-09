a22809b960aeb1fdf2e32f10ecf7138684c0ffa2
Bradey Merrill <bradage.merrill@gmail.com>
2021-10-09 16:33:10 -0600

    mostly working on cart

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const PORT = process.env.PORT || 5000;
 const path = require('path');
 
 const express = require('express');
@@ -5,6 +6,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -20,13 +22,12 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById('616207c73cedaec998a0798f')
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -37,3 +38,34 @@ app.use(errorController.get404);
 mongoConnect(() => {
   app.listen(3000);
 });
+
+// const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+// ...
+// const corsOptions = {
+//     origin: "https://<your_app_name>.herokuapp.com/",
+//     optionsSuccessStatus: 200
+// };
+// app.use(cors(corsOptions));
+
+// const options = {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     family: 4
+// };
+
+// const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
+
+
+// mongoose
+//   .connect(
+//     MONGODB_URL, options
+//   )
+//   .then(result => {
+//     ... // This should be your user handling code implement following the course videos
+//     app.listen(PORT);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
