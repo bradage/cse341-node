4d0c7144184bb3fa5365b958b2a5740ef229048a
Bradey Merrill <bradage.merrill@gmail.com>
2021-10-11 19:46:55 -0600

    adjusting Port

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
 
+
+//URLS, PORTS and CORS options for Heroku
 const corsOptions = {
   origin: "https://bradagecse.herokuapp.com/",
   optionsSuccessStatus: 200
 };
+
+app.use(cors(corsOptions));
+
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://rw:GoeQf8jnWJm9@cluster0-shard-00-00.aomqm.mongodb.net:27017,cluster0-shard-00-01.aomqm.mongodb.net:27017,cluster0-shard-00-02.aomqm.mongodb.net:27017/shop?ssl=true&replicaSet=atlas-3bi6e7-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+//Port for local hose
+const PORT = process.env.PORT || 3000;
 
 const options = {
   useUnifiedTopology: true,
@@ -14,13 +26,7 @@ const options = {
   family: 4
 };
 
-const MONGODB_URL = process.envMONGODB_URL || 'mongodb://rw:GoeQf8jnWJm9@cluster0-shard-00-00.aomqm.mongodb.net:27017,cluster0-shard-00-01.aomqm.mongodb.net:27017,cluster0-shard-00-02.aomqm.mongodb.net:27017/shop?ssl=true&replicaSet=atlas-3bi6e7-shard-0&authSource=admin&retryWrites=true&w=majority';
-
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
+//back to main code
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
@@ -49,8 +55,6 @@ app.use((req, res, next) => {
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(cors(corsOptions));
-
 app.use(errorController.get404);
 
 mongoose
@@ -70,7 +74,7 @@ mongoose
         user.save();
       }
     });
-    app.listen(3000);
+    app.listen(PORT, () => conosle.log(`Listening on localhost:${PORT}`));
   })
   .catch(err => {
     console.log(err);
